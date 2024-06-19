46165bc670b088365ccfd0d5d1f3f3982d122980
Syed Matheen <135738549+Matheen-3@users.noreply.github.com>
2024-06-19 20:05:31 +0530

    student profile page finished

--- student/stuInc/header.php
+++ student/stuInc/header.php
@@ -1,46 +1,37 @@
 <?php
-include_once("db_con.php");
-if(!isset($_SESSION)){
-session_start();
-}
-if(isset($_SESSION["stuLogin"])){
-    $stu_email = $_SESSION['email'];
+// include_once("db_con.php");
+// if(!isset($_SESSION)){
+// session_start();
+// }
+// if(isset($_SESSION["stuLogin"])){
+//     $stu_email = $_SESSION['email'];
 
-    // Fetch student details from database
-    $stmt = $conn->prepare("SELECT * FROM student WHERE stu_email = :stu_email");
-    $stmt->bindParam(':stu_email', $stu_email);
-    $stmt->execute();
+//     // Fetch student details from database
+//     $stmt = $conn->prepare("SELECT * FROM student WHERE stu_email = :stu_email");
+//     $stmt->bindParam(':stu_email', $stu_email);
+//     $stmt->execute();
 
-    // Check if a record exists
-    if($stmt->rowCount() > 0) {
-        $result = $stmt->fetch(PDO::FETCH_ASSOC);
-        $stu_img = $result['stu_img'];
-        $stu_name=$result['stu_name'];
-        // Additional variables from $result can be accessed here
-    } else {
-        // Handle case where no record was found for the logged in student
-        echo '<script>location.href="../index.php";</script>';
-        exit; // Exit to prevent further execution
-    }
-} else {
-    // Redirect if student is not logged in
-    echo '<script>location.href="../index.php";</script>';
-    exit; // Exit to prevent further execution
-}
+//     // Check if a record exists
+//     if($stmt->rowCount() > 0) {
+//         $result = $stmt->fetch(PDO::FETCH_ASSOC);
+//         $stu_img = $result['stu_img'];
+//         $stu_name=$result['stu_name'];
+//         // Additional variables from $result can be accessed here
+//     } else {
+//         // Handle case where no record was found for the logged in student
+//         echo '<script>location.href="../index.php";</script>';
+//         exit; // Exit to prevent further execution
+//     }
+// } else {
+//     // Redirect if student is not logged in
+//     echo '<script>location.href="../index.php";</script>';
+//     exit; // Exit to prevent further execution
+// }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../student\css\style.css">
-</head>
-<body>
+
 <header>
     <div class="logo"><a href="#">SkillyEarn</a></div>
     <div class="panel">Student pannel</div>
     <!-- <div class="profile-img"><img style="width:50px;height:50px; border-radius:100%" src="<?php if(isset($stu_img)) echo $stu_img;?>" alt="<?php if(isset($stu_name)) echo $stu_name?>"></div> -->
     <a href="../logout.php">Logout</a>
 </header>
-</body>
-</html>
--- student/stuInc/my_profile.php
+++ student/stuInc/my_profile.php
@@ -1,27 +1,55 @@
-<div class="right">
-    <h3>My profile</h3>
-    <div class="profile-img"><img style="width:100px;height:100px; border-radius:100%;    border:1px solid blue;
-" src="<?php if(isset($stu_img)) echo $stu_img;?>" alt="<?php if(isset($stu_name)) echo $stu_name?>"></div>
-    <form action="#" method="post" enctype="multipart/form-data">
+<?php
+// Start or resume session
+if(!isset($_SESSION)){
+    session_start();
+}
+// Include database connection
+include("db_con.php");
+
+// Check if session email is set
+if(isset($_SESSION['stuLogin'])){
+    if(isset($_SESSION["email"])) {
+        $email = $_SESSION["email"];
+        
+        // Prepare and execute SQL query to fetch student details
+        $get_details = $conn->prepare("SELECT * FROM student WHERE stu_email=?");
+        $get_details->execute([$email]);
+        $details = $get_details->fetch(PDO::FETCH_ASSOC);
+        
+        // Assign fetched values to variables
+        if($details) {
+
+    ?>
+    
+    <div class="right">
+        <h3>My profile</h3>
+        <div class="profile-img">
+        <img style="width: 100px; height: 100px; border-radius: 50%; border: 1px solid blue;" src="<?php echo '../admin/inc/'.$details['stu_img']; ?>" alt="<?php echo isset($details['stu_name']) ? $details['stu_name'] : 'Student photo'; ?>">
+        </div>
+        <form action="#" method="post" enctype="multipart/form-data">
             <div class="row">
                 <label>Student Name</label>
-                <input type="text" name="stud_name">
+                <input type="text" name="stud_name" value="<?php echo isset($details['stu_name']) ? $details['stu_name'] : ''; ?>">
             </div>
             <div class="row">
                 <label>Email</label>
-                <input type="email" name="stud_email">
+                <input type="email" name="stud_email" value="<?php echo isset($details['stu_email']) ? $details['stu_email'] : ''; ?>" readonly>
             </div>
-            <div class="row">
-                <label>Password</label>
-                <input type="password" name="stud_pass">
-            </div>
+           
             <div class="row">
                 <label>Profile photo</label>
-                <input type="file" name="stud_Img">
+                <input class="fileInp" type="file" name="stud_img">
+                <?php if(isset($stu_img)) { ?>
+                    <img src="<?php echo $details['stu_img']; ?>" alt="student Image" style="max-width: 200px; max-height: 200px;">
+                <?php } ?>
             </div>
             <div class="btns">
-                <button name="addStuBtn" id="addCourse">Update</button>
-
+                <button type="submit" name="updateStuBtn" id="updateStudent">Update</button>
             </div>
-         </form>
-</div>
+        </form>
+    </div>
+ <?php  
+     }
+    }
+ 
+}
